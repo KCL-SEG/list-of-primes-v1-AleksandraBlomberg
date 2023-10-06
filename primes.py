9115c4efa277b1f78e10bbc08c403b000249cbd7
AleksandraBlomberg <146368482+AleksandraBlomberg@users.noreply.github.com>
2023-10-06 15:22:23 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,14 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    counter= 1
+    char = ""
+    frequencies = {}
+    for x in range(0,len(items)):
+        char = items[x]
+        if char in frequencies:
+            count= frequencies.get(char)+1
+            frequencies[char]= count
+        else:
+            frequencies[char]= 1
+    return frequencies
